e797307e4d32d4fb4e671f384bf7fea13e7818d7
Billy Barbaro <billy@thebarbaros.com>
2015-10-26 19:05:40 -0400

    Added file for the home page.

--- ReelTalk/index.ios.js
+++ ReelTalk/index.ios.js
@@ -1,32 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
 var {
   AppRegistry,
+  NavigatorIOS,
   StyleSheet,
-  Text,
-  View,
 } = React;
+
+var HomeScreen = require('./HomeScreen');
 
 var ReelTalk = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Movies',
+          component: HomeScreen,
+        }}
+      />
     );
   }
 });
@@ -34,20 +26,10 @@ var ReelTalk = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: 'white',
   },
 });
 
 AppRegistry.registerComponent('ReelTalk', () => ReelTalk);
+
+module.exports = ReelTalk;

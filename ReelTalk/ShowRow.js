907912cc3d77b49feb2c7dd390d1cc3607ccf762
Billy Barbaro <billy@thebarbaros.com>
2015-10-28 02:27:18 -0400

    Added rows and detail views to top charts

--- ReelTalk/ShowRow.js
+++ ReelTalk/ShowRow.js
@@ -5,20 +5,36 @@ var {
   AppRegistry,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View,
 } = React;
 
+var MovieDetailView = require("./MovieDetailView");
+
 var ShowRow = React.createClass({
+  showDetails: function(show) {
+    this.props.navigator.push({
+      title: show.name,
+      component: MovieDetailView,
+      passProps: {
+        initialShow: show,
+        navigator: this.props.navigator,
+      },
+    });
+  },
+
   render: function() {
     return (
-      <Text>{this.props.show.name}</Text>
+      <TouchableHighlight style={styles.container} onPress={()=>this.showDetails(this.props.show)}>
+        <Text>{this.props.show.name}</Text>
+      </TouchableHighlight>
     );
   },
 });
 
 var styles = StyleSheet.create({
   container: {
-    height: 20,
+    height: 100,
     backgroundColor: 'white',
   },
 });

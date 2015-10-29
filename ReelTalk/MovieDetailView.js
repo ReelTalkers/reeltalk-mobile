2d046d78de9cb948b19067830a8335f53d39901b
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2015-10-28 23:56:00 -0400

    Add fade and text styling
    Signed-off-by: Andrew Hamm <AndrewTurner.Hamm@gmail.com>

--- ReelTalk/MovieDetailView.js
+++ ReelTalk/MovieDetailView.js
@@ -38,22 +38,29 @@ var MovieDetailView = React.createClass({
         <Image
             source={{uri: this.state.show.largePoster}}
             style={styles.largeImage}
-        />
-        <View style={styles.metadata}>
-          <Text>Name: {this.state.show.name}</Text>
-          <Text>Year: {this.state.show.year}</Text>
-          <Text>Runtime: {this.state.show.runtime}</Text>
-          <Text>Rating: {this.state.show.rating}</Text>
-          <Text>Genre: {this.state.show.genre}</Text>
-          <Text>Director: {this.state.show.director}</Text>
-          <Text style={styles.actors}>Actors</Text>
-          {this.state.show.actors.map(actor => <Text>{actor}</Text>)}
+        >
+          <View style={styles.summary}>
+            <View style={[styles.summaryTriangle, styles.summaryTriangleLeft]} />
+            <View style={[styles.summaryTriangle, styles.summaryTriangleRight]} />
+            <View style={[styles.summaryInfo, styles.paletteBackground]}>
+              <Text style={[styles.name, styles.paletteFont]}>{this.state.show.name}</Text>
+              <Text style={[styles.summaryDetail, styles.paletteDetailFont]}>{this.state.show.year} - {this.state.show.runtime}</Text>
+            </View>
+          </View>
+        </Image>
+        <View style={[styles.content, styles.paletteBackground]}>
+          <View style={styles.metadata}>
+            <Text style={styles.paletteFont}>Rating: {this.state.show.rating}</Text>
+            <Text style={styles.paletteFont}>Genre: {this.state.show.genre}</Text>
+            <Text style={styles.paletteFont}>Director: {this.state.show.director}</Text>
+            <Text style={[styles.actors, styles.paletteFont]}>Actors</Text>
+            {this.state.show.actors.map(actor => <Text style={styles.paletteFont}>{actor}</Text>)}
+          </View>
+          <Text style={styles.paletteFont}>Description: {this.state.show.description}</Text>
+          <Rating averageRating={this.state.show.averageRating}/>
+          <Text style={styles.paletteFont}>Viewers also Enjoyed</Text>
+          <LolomoRow header={"Others also enjoyed:"} category={json.categories[0]} onSelect={this._changeShow}/>
         </View>
-        <Text>Description: {this.state.show.description}</Text>
-        <Rating averageRating={this.state.show.averageRating}/>
-        <Text>Viewers also Enjoyed</Text>
-        <LolomoRow header={"Others also enjoyed:"} category={json.categories[0]} onSelect={this._changeShow}/>
-
       </ScrollView>
     );
   },
@@ -66,13 +73,76 @@ var styles = StyleSheet.create({
     actors: {
       textDecorationLine: 'underline',
     },
-    metadata: {
-      flexDirection: 'column',
-    },
     largeImage: {
       flex: 1,
+      overflow: 'hidden',
       width: 375,
-      height: 600,
+      height: 525,
+    },
+    summary: {
+      position: 'absolute',
+      bottom: 0,
+    },
+    summaryInfo: {
+      // dont like that the width is hard coded
+      width: 375,
+      paddingLeft: 18,
+      paddingTop: 3,
+      paddingBottom: 3,
+      shadowColor: "rgba(208, 206, 184, 1)",
+      shadowOpacity: 1,
+      shadowRadius: 10,
+      shadowOffset: {
+        height: -25,
+        width: 0,
+      }
+    },
+    name: {
+      fontSize: 22,
+    },
+    summaryDetail: {
+      fontSize: 13,
+      fontWeight: "300",
+    },
+    paletteBackground: {
+      backgroundColor: 'rgba(208, 206, 184, 1)',
+    },
+    paletteFont: {
+      color: 'rgb(57, 35, 29)',
+    },
+    paletteDetailFont: {
+      color: 'rgb(169, 116, 56)',
+    },
+    summaryTriangle: {
+      backgroundColor: 'transparent',
+      borderTopWidth: 30,
+      borderRightWidth: 0,
+      borderBottomWidth: 0,
+      borderLeftWidth: 150,
+      borderTopColor: 'transparent',
+      borderBottomColor: 'transparent',
+      borderRightColor: 'transparent',
+      borderLeftColor: 'rgba(208, 206, 184, 1)',
+      shadowColor: "rgba(208, 206, 184, 1)",
+      shadowOpacity: 1,
+      shadowRadius: 5,
+      shadowOffset: {
+        height: -15,
+        width: -8,
+      }
+    },
+    summaryTriangleLeft: {
+      position: 'absolute',
+      top: -20,
+      left: 0
+    },
+    summaryTriangleRight: {
+      position: 'absolute',
+      top: -20,
+      right: 0,
+      transform: [
+        {scaleX: -1}
+      ]
     },
 });
 

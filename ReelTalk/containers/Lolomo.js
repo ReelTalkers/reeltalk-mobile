f61795875753b14095700ae25ef1aae9d3686021
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2015-10-29 22:25:48 -0400

    Explicitly set lolomo adjustContentInsets to true.

--- ReelTalk/containers/Lolomo.js
+++ ReelTalk/containers/Lolomo.js
@@ -56,6 +56,7 @@ var Lolomo = React.createClass({
         dataSource={this.state.dataSource}
         renderRow={this.renderLolomoRow}
         style={styles.listView}
+        automaticallyAdjustContentInsets={true}
         showsVerticalScrollIndicator={false}
       />
     );

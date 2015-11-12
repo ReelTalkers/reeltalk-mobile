f3bdb9855f4db8d2773d686dc309cfd070ede9c1
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2015-11-12 00:58:39 -0500

    Add flashing header
    Signed-off-by: Andrew Hamm <AndrewTurner.Hamm@gmail.com>

--- ReelTalk/components/HeaderScrollView.js
+++ ReelTalk/components/HeaderScrollView.js
@@ -30,7 +30,7 @@ var HeaderScrollView = React.createClass({
 
     getInitialState: function () {
         return {
-            scrollY: new Animated.Value(0)
+            scrollingUp: true,
         };
     },
 
@@ -46,7 +46,10 @@ var HeaderScrollView = React.createClass({
       this._scrollView.setNativeProps(props);
     },
 
-    renderBackground: function () {
+    renderBackground: function (isHidden) {
+        if (isHidden) {
+          return null;
+        }
         var { windowHeight, backgroundSource } = this.props;
         var { scrollY } = this.state;
         if (!windowHeight || !backgroundSource) {
@@ -56,16 +59,26 @@ var HeaderScrollView = React.createClass({
             <Animated.Image
                 style={[styles.background, {
                     height: windowHeight,
-                    transform: [{
-                        translateY: scrollY.interpolate({
-                            inputRange: [-windowHeight, 0, windowHeight],
-                            outputRange: [windowHeight, 0, -windowHeight/3]
-                        })
-                    }]
+                    // transform: [{
+                    //     translateY: scrollY.interpolate({
+                    //         inputRange: [-windowHeight, 0, windowHeight],
+                    //         outputRange: [0, 0, windowHeight]
+                    //     })
+                    // }],
                 }]}
                 source={backgroundSource}
             />
         );
+    },
+
+    renderScrollBackground: function() {
+      // when you are scrolling up, this should be hidden
+      return this.renderBackground(this.state.scrollingUp);
+    },
+
+    renderStuckBackground: function() {
+      // when you are scrolling up this should be shown
+      return this.renderBackground(!this.state.scrollingUp);
     },
 
     renderHeader: function () {
@@ -78,30 +91,46 @@ var HeaderScrollView = React.createClass({
             <Animated.View style={{
                 position: 'relative',
                 height: windowHeight,
-                backgroundColor: 'black',
-                opacity: scrollY.interpolate({
-                    inputRange: [-windowHeight, 0, windowHeight * 1.2],
-                    outputRange: [0, 0, 1]
-                }),
+                // backgroundColor: 'black',
+                // opacity: scrollY.interpolate({
+                //     inputRange: [-windowHeight, 0, windowHeight * 1.2],
+                //     outputRange: [0, 0, 1]
+                // }),
             }}>
                 {this.props.header}
             </Animated.View>
         );
     },
 
+    handleScroll: function(event: Object) {
+      var scrollY = event.nativeEvent.contentOffset.y;
+      var scrollingUp = this.state.scrollingUp;
+      if (scrollY > 0) {
+        scrollingUp = true;
+      } else {
+        scrollingUp = false;
+      }
+      // only change it if you need to - avoids unnecessary rerendering
+      if (scrollingUp != this.state.scrollingUp) {
+        this.setState({
+          scrollingUp: scrollingUp,
+        })
+      }
+    },
+
     render: function () {
         var { style, ...props } = this.props;
         return (
             <View style={[styles.container, style]}>
-                {this.renderBackground()}
+                {this.renderStuckBackground()}
                 <ScrollView
                     ref={component => { this._scrollView = component; }}
                     {...props}
                     style={styles.scrollView}
-                    onScroll={Animated.event(
-                      [{ nativeEvent: { contentOffset: { y: this.state.scrollY }}}]
-                    )}>
+                    onScroll={this.handleScroll}
+                    scrollEventThrottle={16}>
                     {this.renderHeader()}
+                    {this.renderScrollBackground()}
                     <View style={styles.content}>
                         {this.props.children}
                     </View>
@@ -120,7 +149,8 @@ var styles = StyleSheet.create({
     },
     background: {
         position: 'absolute',
-        backgroundColor: '#2e2f31',
+        top: 0,
+        //backgroundColor: '#2e2f31',
         width: screen.width,
         resizeMode: 'cover'
     },

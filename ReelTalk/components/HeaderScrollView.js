105a57e5b86bbe17faef2df921a9f3f13919aeb4
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2015-11-11 21:35:44 -0500

    Add sloppy start
    Signed-off-by: Andrew Hamm <AndrewTurner.Hamm@gmail.com>

---
+++ ReelTalk/components/HeaderScrollView.js
@@ -0,0 +1,111 @@
+'use strict';
+
+var React = require('react-native');
+var {
+    StyleSheet,
+    View,
+    ScrollView,
+    Animated
+    } = React;
+
+var ScrollableMixin = require('react-native-scrollable-mixin');
+
+var screen = require('Dimensions').get('window');
+var ScrollViewPropTypes = ScrollView.propTypes;
+
+var HeaderScrollView = React.createClass({
+    mixins: [ScrollableMixin],
+
+    propTypes: {
+        ...ScrollViewPropTypes,
+        windowHeight: React.PropTypes.number,
+        backgroundSource: React.PropTypes.object,
+    },
+
+    getDefaultProps: function () {
+        return {
+            windowHeight: 300,
+        };
+    },
+
+    getInitialState: function () {
+        return {
+            scrollY: new Animated.Value(0)
+        };
+    },
+
+    /**
+     * IMPORTANT: You must return the scroll responder of the underlying
+     * scrollable component from getScrollResponder() when using ScrollableMixin.
+     */
+    getScrollResponder() {
+      return this._scrollView.getScrollResponder();
+    },
+
+    setNativeProps(props) {
+      this._scrollView.setNativeProps(props);
+    },
+
+    renderBackground: function () {
+        var { windowHeight, backgroundSource, blur } = this.props;
+        var { scrollY } = this.state;
+        if (!windowHeight || !backgroundSource) {
+            return null;
+        }
+        return (
+            <Animated.Image
+                style={[styles.background, {
+                    height: windowHeight,
+                    transform: [{
+                        translateY: scrollY.interpolate({
+                            inputRange: [-windowHeight, 0, windowHeight],
+                            outputRange: [windowHeight, 0, -windowHeight/3]
+                        })
+                    }]
+                }]}
+                source={backgroundSource}
+            />
+        );
+    },
+
+    render: function () {
+        var { style, ...props } = this.props;
+        return (
+            <View style={[styles.container, style]}>
+                {this.renderBackground()}
+                <ScrollView
+                    ref={component => { this._scrollView = component; }}
+                    {...props}
+                    style={styles.scrollView}
+                    onScroll={Animated.event(
+                      [{ nativeEvent: { contentOffset: { y: this.state.scrollY }}}]
+                    )}>
+                    <View style={styles.content}>
+                        {this.props.children}
+                    </View>
+                </ScrollView>
+            </View>
+        );
+    }
+});
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    scrollView: {
+        backgroundColor: 'transparent',
+    },
+    background: {
+        position: 'absolute',
+        backgroundColor: '#2e2f31',
+        width: screen.width,
+        resizeMode: 'cover'
+    },
+    content: {
+        flex: 1,
+        flexDirection: 'column'
+    }
+});
+
+module.exports = HeaderScrollView;

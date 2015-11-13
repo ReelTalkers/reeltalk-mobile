2da382e5a9aeca9ec16a12cb3059b86fc8aaccb9
Greg Ziegan <greg.ziegan@gmail.com>
2015-11-13 01:19:27 -0500

    still trying to get tests running

---
+++ ReelTalk/components/__tests__/star-test.js
@@ -0,0 +1,11 @@
+jest.dontMock('../Star');
+
+var Star = require('../Star');
+
+describe('Star', function() {
+  describe('checkColor', function() {
+    it('is the right color on press', function() {
+      "1" == "1"
+    });
+  });
+});

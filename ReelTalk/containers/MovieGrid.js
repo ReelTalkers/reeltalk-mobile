6fde7ee40b9af803ff2839f7c73241c6fd07315a
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2015-11-18 22:59:58 -0500

    Add fix for 3 scaled images.

--- ReelTalk/containers/MovieGrid.js
+++ ReelTalk/containers/MovieGrid.js
@@ -12,6 +12,8 @@ var {
 } = React;
 
 var json = require("../Data");
+var screen = require('Dimensions').get('window');
+
 var MovieDetailView = require("./MovieDetailView");
 
 var MovieGrid = React.createClass({
@@ -52,27 +54,42 @@ var MovieGrid = React.createClass({
 
   render: function() {
     return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderGridComponent}
-        style={this.listView}
-        contentContainerStyle={styles.listViewContainer}
-        automaticallyAdjustContentInsets={false}
-      />
+      <View style={styles.container}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderGridComponent}
+          style={this.listView}
+          contentContainerStyle={styles.listViewContainer}
+          automaticallyAdjustContentInsets={false}
+        />
+      </View>
     );
   },
 });
 
+// TODO: not sure that this is the best way to do it, but cant think of how to use flexbox and maintain ratio
+// enough space for 3 images and padding arround them
+var posterMargin = 5;
+// left and right of all 3 images and on wrapper = 8
+var totalSpacing = posterMargin * 8;
+var posterWidth = (screen.width - totalSpacing) / 3;
+var posterRatio = 172 / 115;
+
 var styles = StyleSheet.create({
+  container: {
+    margin: posterMargin,
+    // TODO: height only temp fix for bug
+    height: screen.height,
+  },
   listView: {
     flex: 1,
   },
   listViewContainer: {
     flexDirection: 'row',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
   },
   movieButton: {
-    margin: 5,
+    margin: posterMargin,
     shadowColor: "black",
     shadowOpacity: 0.35,
     shadowRadius: 2,
@@ -82,8 +99,8 @@ var styles = StyleSheet.create({
     }
   },
   image: {
-    width: 115,
-    height: 172,
+    width: posterWidth,
+    height: posterWidth * posterRatio,
   },
 });
 

47ff986496abaeb47082b41533cb35d906b4e2e2
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2015-11-19 23:26:58 -0500

    Show number of movies in a list.

--- ReelTalk/containers/ListsHome.js
+++ ReelTalk/containers/ListsHome.js
@@ -71,7 +71,7 @@ class ListsHome extends React.Component {
             {this._getListImage(list.shows.edges)}
             <View>
               <Text style={styles.listTitle}>{list.title}</Text>
-              <Text style={styles.listSubheading}>{list.shows.edges.totalCount} items</Text>
+              <Text style={styles.listSubheading}>{list.shows.edges.length} items</Text>
             </View>
           </View>
           <View style={styles.rowDivider}/>
